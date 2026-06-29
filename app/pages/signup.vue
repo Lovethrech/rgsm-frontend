bb4952b712d892b49dd4d1efb2705f639869b170
Lovethrech <rachealdolapo45@gmail.com>
2026-06-30 00:20:21 +0100

    ..working on auth page.

---
+++ app/pages/signup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="auth display-flex-center-center">
+        <Auth/>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: "Auth | RGSM Dashboard"
+})
+</script>
+<style scoped>
+.auth{
+    width: 100%;
+    height: 100vh;
+    line-height: 2;
+    background: url("../assets/LandingPagePattern.svg") center/auto no-repeat;
+    background-size: cover;
+}
+</style>
