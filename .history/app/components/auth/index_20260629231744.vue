5075434b4002f3a054d52a2ca5e37bc07abb10cd
Lovethrech <rachealdolapo45@gmail.com>
2026-06-29 23:20:28 +0100

    ..starting the project.

---
+++ .history/app/components/auth/index_20260629231744.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="auth-main glass">
+        <h1 class="auth-main-title">
+            Welcome, Admin
+        </h1>
+        <p class="auth-main-desc">
+            To access the realtime system dashboard for monitoring and moderation.
+        </p>
+        <br/>
+        <div class="auth-main-form-ct">
+            <AuthLogin/>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.auth-main{
+    padding: 3vh;
+    margin: 0 3vh;
+}
+.auth-main-title, .auth-main-desc{
+    text-align: center;
+}
+.auth-main-title{
+    color: #93C5FD; 
+    font-weight:900;
+}
+.auth-main-desc{
+    color: #9ca3af;
+}
+</style>
