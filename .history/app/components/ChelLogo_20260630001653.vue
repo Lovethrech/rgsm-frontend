262418a67af7c4af0499b236ea6efac6b14163d6
Lovethrech <rachealdolapo45@gmail.com>
2026-06-30 00:17:27 +0100

    ..working on login page.

---
+++ .history/app/components/ChelLogo_20260630001653.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="chel-logo-ctn">
+        <img src="~/assets/ChelLogoImg.png" alt="Chel Logo" class="chel-logo"/>
+    </div>
+</template>
+
+<style scoped>
+.chel-logo-ctn{
+    width: 18vh;
+    margin: 0 auto;
+}
+.chel-logo-ctn img{
+    width: 100%;
+    height: auto;
+}
+</style>
